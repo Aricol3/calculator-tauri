493660b4f201d5936270fc7e310b54a60469bfee
adrian <adriansurdu33@gmail.com>
2024-10-16 02:12:23 +0300

    small fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,8 +25,8 @@ pub fn run() {
                         .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
                     #[cfg(target_os = "windows")]
-                    apply_acrylic(&window, Some((18, 18, 18, 125)))
-                        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+                    apply_acrylic(&window, Some((0, 0, 0, 125)))
+                        .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
 
                     Ok(())
                 })
