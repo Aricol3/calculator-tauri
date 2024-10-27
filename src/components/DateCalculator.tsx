621006d7b68695a3e5331d84ce9e6a1a7225f052
Adrian <adriansurdu33@gmail.com>
2024-10-27 18:52:32 +0200

    some date calculator design

--- src/components/DateCalculator.tsx
+++ src/components/DateCalculator.tsx
@@ -1,12 +1,17 @@
 import {
-  DateRangePicker,
+  DateRangePicker, Divider, TimeInput
 } from "@nextui-org/react";
 
 const DateCalculator = () => {
 
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
-      <DateRangePicker className="dark" variant="bordered" color="secondary" />
+      <DateRangePicker className="dark" variant="underlined" label="Date range" />
+      <Divider />
+      <div className="flex flex-wrap gap-4">
+        <TimeInput label="Start Time" variant="bordered" />
+        <TimeInput label="End Time" variant="bordered" />
+      </div>
     </div>
   );
 };
